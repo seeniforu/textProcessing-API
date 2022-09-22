b373d414b98a79002b2133184e7bd6c29e5c213e
Srinivasan <srinivasanforu7@gmail.com>
2022-09-22 08:01:52 +0530

    API for text processing

---
+++ temp.py
@@ -0,0 +1,20 @@
+from unicodedata import name
+from fastapi import FastAPI
+import uvicorn
+from TextProcessing import output
+
+app = FastAPI()
+
+@app.get('/') 
+def helloWorld():
+    return f"hello It's working!"
+
+@app.get('/index') 
+def helloWorld(name : str):
+    out = output.resultGeneration(name)
+    return f"{out}!"
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host='0.0.0.0')
+
