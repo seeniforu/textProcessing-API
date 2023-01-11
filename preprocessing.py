fad34942c2085bd87d78f9c79ce08748f2d7162e
Srinivasan <srinivasanforu7@gmail.com>
2023-01-11 14:06:53 +0530

    dtype update

--- preprocessing.py
+++ preprocessing.py
@@ -66,7 +66,7 @@ import random
 import numpy as np
 
 random.shuffle(training)             
-training = np.array(training)     
+training = np.array(training, dtype=int)     
 train_x = list(training[:,0])        
 train_y = list(training[:,1])
 print("Training data created")
